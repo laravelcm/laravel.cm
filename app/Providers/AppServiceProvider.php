11ed5f5a36e24968928c10bf5357a4efcb83e17e
Arthur Monney <monneylobe@gmail.com>
2021-05-05 16:37:17 +0100

    :sparkles: ajout de la macro pour la temps de lecture d'un article

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Str::macro('readDuration', function (...$text) {
+            $totalWords = str_word_count(implode(" ", $text));
+            $minutesToRead = round($totalWords / 200);
+
+            return (int) max(1, $minutesToRead);
+        });
     }
 }
