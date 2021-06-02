a97a0deafea4104fce2074aa35dde60e19a91fe7
Arthur Monney <monneylobe@gmail.com>
2021-06-02 10:21:45 +0100

    :lipstick: Ajout des composants pour le site et mise en place ds variables sur la landing

---
+++ resources/views/layouts/_fathom.blade.php
@@ -0,0 +1,11 @@
+@production
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-J6PRLE9BBY"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-J6PRLE9BBY');
+    </script>
+@endproduction
