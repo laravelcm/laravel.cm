578412f88f346fe7b7bdeef47058d1d03f5643de
Arthur Monney <monneylobe@gmail.com>
2021-06-03 18:48:11 +0100

    :bento: Suppression de la configuration concernant le poids de la police (font-weight)

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,18 +16,6 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontWeight: {
-      hairline: 100,
-      'extra-light': 100,
-      light: 200,
-      normal: 300,
-      medium: 500,
-      semibold: 600,
-      bold: 700,
-      extrabold: 800,
-      'extra-bold': 800,
-      black: 900,
-    },
     extend: {
       colors: {
         brand: {
