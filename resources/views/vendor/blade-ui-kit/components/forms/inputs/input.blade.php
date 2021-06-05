f5746d64488596f28d9cb6f9044263c7677ffdb7
Arthur Monney <monneylobe@gmail.com>
2021-06-05 09:09:25 +0100

    :lipstick: Modification des composants blade ui kit

--- resources/views/vendor/blade-ui-kit/components/forms/inputs/input.blade.php
+++ resources/views/vendor/blade-ui-kit/components/forms/inputs/input.blade.php
@@ -2,7 +2,7 @@
     <div class="relative">
         @if ($attributes->get('prefix-icon'))
             <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                @svg($attributes->get('prefix-icon'), ['class' => 'h-5 w-5 text-gray-400'])
+                @svg($attributes->get('prefix-icon'), ['class' => 'h-5 w-5 text-skin-muted'])
             </div>
         @endif
 
@@ -12,7 +12,7 @@
             id="{{ $id }}"
             @if ($value)value="{{ $value }}"@endif
             {{ $attributes->merge([
-                'class' => 'bg-skin-input shadow-sm focus:ring-flag-green focus:border-flag-green block w-full placeholder-skin-input focus:outline-none focus:placeholder-skin-input-focus font-normal text-skin-base sm:text-sm border-skin-input rounded-md' . ($attributes->get('prefix-icon') ? ' pl-10' : '') . ($errors->has($name) ? ' border-red-300 text-red-900 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500' : '')
+                'class' => 'bg-skin-input shadow-sm focus:ring-flag-green focus:border-flag-green block w-full placeholder-skin-input focus:outline-none focus:placeholder-skin-input-focus font-normal text-skin-base sm:text-sm border-skin-input rounded-md' . ($attributes->get('prefix-icon') ? ' pl-10' : '') . ($errors->has($name) ? ' border-red-300 text-red-500 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500' : '')
             ]) }}
         />
 
