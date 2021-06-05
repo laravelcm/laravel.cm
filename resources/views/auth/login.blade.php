e82ef4f19fe16f1faf2b02d528074e4a1c204f29
Arthur Monney <monneylobe@gmail.com>
2021-06-05 09:28:35 +0100

    :lipstick: Ajout des pages d'authentification

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
                     {{ __('Se connecter à son compte') }}
                 </h2>
             </div>
-            <form class="space-y-6" action="#" method="POST">
+            <form class="space-y-6" action="{{ route('login') }}" method="POST">
                 @csrf
                 <div class="rounded-md shadow-sm -space-y-px">
                     <div>
@@ -27,13 +27,13 @@
                 <div class="flex items-center justify-between">
                     <div class="flex items-center">
                         <input id="remember_me" name="remember_me" type="checkbox" class="h-4 w-4 text-green-600 bg-skin-input focus:ring-green-500 border-skin-input rounded" />
-                        <label for="remember_me" class="ml-2 block text-sm text-skin-base font-normal">
+                        <label for="remember_me" class="ml-2 block text-sm text-skin-base font-normal cursor-pointer">
                             {{ __('Se souvenir de moi') }}
                         </label>
                     </div>
 
                     <div class="text-sm">
-                        <a href="#" class="font-medium text-green-600 hover:text-green-500">
+                        <a href="{{ route('password.request') }}" class="font-medium text-green-600 hover:text-green-500">
                             {{ __('Mot de passe oublié?') }}
                         </a>
                     </div>
@@ -51,7 +51,7 @@
             <div>
                 <div class="relative">
                     <div class="absolute inset-0 flex items-center">
-                        <div class="w-full border-t border-skin"></div>
+                        <div class="w-full border-t border-skin-light"></div>
                     </div>
                     <div class="relative flex justify-center text-sm">
                         <span class="px-2 bg-skin-body text-skin-base font-normal">
@@ -62,7 +62,7 @@
 
                 <div class="mt-6 grid grid-cols-2 gap-2">
                     <div>
-                        <a href="#" class="w-full inline-flex justify-center py-2 px-4 border border-skin rounded-md shadow-sm bg-skin-button text-sm font-normal text-skin-base hover:bg-skin-button-hover">
+                        <a href="#" class="w-full inline-flex justify-center py-2 px-4 border border-skin-light rounded-md shadow-sm bg-skin-button text-sm font-normal text-skin-base hover:bg-skin-button-hover">
                             <span class="sr-only">{{ __('Créer son compte avec Github') }}</span>
                             <svg class="w-5 h-5 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
                                 <path fill-rule="evenodd" d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z" clip-rule="evenodd"></path>
@@ -72,7 +72,7 @@
                     </div>
 
                     <div>
-                        <a href="#" class="w-full inline-flex justify-center py-2 px-4 border border-skin rounded-md shadow-sm bg-skin-button text-sm font-normal text-skin-base hover:bg-skin-button-hover">
+                        <a href="#" class="w-full inline-flex justify-center py-2 px-4 border border-skin-light rounded-md shadow-sm bg-skin-button text-sm font-normal text-skin-base hover:bg-skin-button-hover">
                             <span class="sr-only">{{ __('Créer son compte avec Gmail') }}</span>
                             <svg  class="w-5 h-5 mr-2 -ml-1" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                 <g clip-path="url(#clip0)">
