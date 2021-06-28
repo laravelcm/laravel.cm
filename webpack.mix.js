d3da6bfd64da558f78c46dcfabf3b7688b3a0fe2
Arthur Monney <monneylobe@gmail.com>
2021-06-28 22:32:52 +0100

    :package: Migration sur Tailwind 2.2.4 et Alpine v3

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,9 +12,10 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-  .postCss("resources/css/app.css", "public/css").options({
+  .postCss('resources/css/app.css', 'public/css').options({
     postCss: [
-      require("@tailwindcss/jit"),
+      require('tailwindcss'),
+      require('autoprefixer'),
     ],
 });
 
