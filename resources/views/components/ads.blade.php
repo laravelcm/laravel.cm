60cb538159b7971450ccad26f471784f30a2d77d
Arthur Monney <monneylobe@gmail.com>
2021-08-18 12:12:58 +0100

    :lipstick: ajout des composants pour les articles

---
+++ resources/views/components/ads.blade.php
@@ -0,0 +1,13 @@
+<div id="laravelcm-ads" class="mt-14">
+    <a href="https://github.com/mckenziearts/laravel-notify/?utm_source=laravelcm&amp;utm_medium=sidebar-widget" class="block">
+        <img class="rounded-lg" src="https://laravel.cm/images/notify.png?25f8fddc09fe5e9f273edaa3ba02bd39" alt="Laravel Notify" width="457" height="336">
+    </a>
+    <p class="mt-4 font-normal">
+        <a href="https://github.com/mckenziearts/laravel-notify/?utm_source=laravelcm&amp;utm_medium=sidebar-widget" class="text-sm leading-5 text-skin-base">
+            Découvrez la nouvelle version de Laravel Notify pour vos projets Laravel.
+        </a>
+    </p>
+    <div class="mt-2">
+        <a href="https://github.com/mckenziearts/laravel-notify/?utm_source=laravelcm&amp;utm_medium=sidebar-widget" class="text-sm text-skin-primary hover:text-skin-primary-hover font-medium hover:underline">En savoir plus →</a>
+    </div>
+</div>
---
+++ resources/views/components/articles/overview.blade.php
@@ -0,0 +1,49 @@
+<div class="space-y-4 lg:grid lg:grid-cols-3 lg:items-start lg:gap-6 lg:space-y-0">
+    <a href="#">
+        <div class="aspect-w-3 aspect-h-2">
+            <img class="object-cover shadow-lg rounded-lg" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="">
+        </div>
+    </a>
+    <div class="sm:col-span-2">
+        <div class="flex items-center space-x-2">
+            <span class="inline-flex items-center leading-none px-2.5 py-1 text-sm font-medium text-skin-inverted rounded-full border border-skin-input font-sans">
+                <svg class="mr-1.5 h-2 w-2 brand-applications" fill="currentColor" viewBox="0 0 8 8">
+                    <circle cx="4" cy="4" r="3" />
+                </svg>
+                Applications
+            </span>
+            <span class="inline-flex items-center leading-none px-2.5 py-1 text-sm font-medium text-skin-inverted rounded-full border border-skin-input font-sans">
+                <svg class="mr-1.5 h-2 w-2 brand-tailwind" fill="currentColor" viewBox="0 0 8 8">
+                    <circle cx="4" cy="4" r="3" />
+                </svg>
+                Tailwind
+            </span>
+        </div>
+        <div class="mt-2">
+            <a href="#" class="group">
+                <h4 class="font-xl leading-7 font-bold font-sans text-skin-inverted group-hover:text-skin-primary">Ameliorer votre experience utilisateur</h4>
+            </a>
+            <p class="mt-1 text-sm font-normal text-skin-base leading-5">The standard error pages in Laravel are looking a bit awful. In this tutorial I will show you how yo... </p>
+            <div class="mt-3 flex items-center font-sans">
+                <div class="flex-shrink-0">
+                    <a href="#">
+                        <span class="sr-only">Arthur Monney</span>
+                        <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                    </a>
+                </div>
+                <div class="ml-3">
+                    <p class="text-sm font-medium text-skin-inverted">
+                        <a href="#" class="hover:underline">
+                            Arthur Monney
+                        </a>
+                    </p>
+                    <div class="flex space-x-1 text-sm text-skin-muted">
+                        <time datetime="2020-03-16">Mar 16, 2020</time>
+                        <span aria-hidden="true">&middot;</span>
+                        <span>6 min de lecture</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
