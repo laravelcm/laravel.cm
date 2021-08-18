beb4e67041006ccfdb8a75384a04cdbe18ce0dc8
Arthur Monney <monneylobe@gmail.com>
2021-08-18 14:14:13 +0100

    :card_file_box: wip tests unitaires sur les articles

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Article::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => $this->faker->sentence,
+            'body' => $this->faker->paragraphs(3, true),
+            'slug' => $this->faker->unique()->slug,
+        ];
+    }
+}
