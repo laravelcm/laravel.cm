b237f8e3616832a183fb43d0e883317723bd7270
Arthur Monney <monneylobe@gmail.com>
2021-08-18 13:49:15 +0100

    :card_file_box: Mise en place de la migration et du model pour les articles

---
+++ database/migrations/2021_08_18_115000_create_articles_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('body');
+            $table->string('slug')->unique();
+            $table->string('cover_image')->nullable();
+            $table->string('canonical_url')->nullable();
+            $table->boolean('show_toc')->default(false);
+            $table->boolean('is_pinned')->default(false);
+            $table->boolean('is_sponsored')->default(false);
+            $table->unsignedBigInteger('tweet_id')->nullable();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->timestamp('submitted_at')->nullable();
+            $table->timestamp('approved_at')->nullable();
+            $table->timestamp('shared_at')->nullable();
+            $table->timestamp('sponsored_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
