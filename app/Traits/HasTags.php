6ae35c3d48dfca7f4a90fdde0d80466e78103f35
Arthur Monney <monneylobe@gmail.com>
2021-08-18 13:49:46 +0100

    :card_file_box: Creation du helper pour la gestion des tags

--- app/Traits/HasTags.php
+++ app/Traits/HasTags.php
@@ -2,7 +2,28 @@
 
 namespace App\Traits;
 
-class HasTags
-{
+use App\Models\Tag;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
+trait HasTags
+{
+    public function syncTags(array $tags)
+    {
+        $this->save();
+        $this->tags()->sync($tags);
+
+        $this->unsetRelation('tags');
+    }
+
+    public function removeTags()
+    {
+        $this->tags()->detach();
+
+        $this->unsetRelation('tags');
+    }
+
+    public function tags(): MorphToMany
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
+    }
 }
