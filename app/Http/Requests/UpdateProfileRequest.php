8ecd1f67993f806fade85deb645a515db3027be1
Arthur Monney <monneylobe@gmail.com>
2021-10-16 21:33:33 +0100

    modification des regles de validation pour la modification du compte

--- app/Http/Requests/UpdateProfileRequest.php
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -33,8 +33,8 @@ class UpdateProfileRequest extends FormRequest
             'username' => 'required|alpha_dash|max:255|unique:users,username,' . Auth::id(),
             'twitter_profile' => 'max:255|nullable|unique:users,twitter_profile,' . Auth::id(),
             'github_profile' => 'max:255|nullable|unique:users,github_profile,' . Auth::id(),
-            'bio' => 'max:160',
-            'website' => 'url',
+            'bio' => 'nullable|max:160',
+            'website' => 'nullable|url',
             'avatar' => $this->validateSingleMedia()
                 ->extension(['png', 'jpg', 'jpeg'])
                 ->maxItemSizeInKb(1024),
