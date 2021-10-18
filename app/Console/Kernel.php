ba3008ba2062de6fa1584a3c46e4b058cdbf640a
Arthur Monney <monneylobe@gmail.com>
2021-10-18 16:03:11 +0100

    :mag: Ajout de la generation du sitemap.xml

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('media-library:delete-old-temporary-uploads')->daily();
+        $schedule->command('sitemap:generate')->daily();
     }
 
     /**
