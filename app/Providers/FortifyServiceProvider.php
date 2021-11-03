74c65a830e59cb1673d25a72d5a9a96d91f1455b
Arthur Monney <monneylobe@gmail.com>
2021-11-03 16:29:52 +0100

    :sparkles: Ajout de la verification d'email

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -35,6 +35,7 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::registerView(fn () => view('auth.register'));
         Fortify::requestPasswordResetLinkView(fn () => view('auth.forgot-password'));
         Fortify::resetPasswordView(fn ($request) => view('auth.reset-password', ['request' => $request]));
+        Fortify::verifyEmailView(fn () => view('auth.verify-email'));
 
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
---
+++ resources/views/auth/verify-email.blade.php
@@ -0,0 +1,57 @@
+@title('Vérification de l\'adresse e-mail')
+
+@extends('layouts.master')
+
+@section('content')
+
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+        <div>
+            <img class="h-12 w-auto sm:h-16 logo-white" src="{{ asset('/images/laravelcm.svg') }}" alt="Laravel.cm">
+            <img class="h-12 w-auto sm:h-16 logo-dark" src="{{ asset('/images/laravelcm-white.svg') }}" alt="Laravel.cm">
+        </div>
+
+        <div class="w-full sm:max-w-md lg:max-w-xl mt-6 lg:mt-10">
+            <div class="mb-4 text-sm text-skin-base font-normal text-center">
+                {{ __('Merci pour votre inscription! Avant de commencer, pourriez-vous vérifier votre adresse e-mail en cliquant sur le lien que nous venons de vous envoyer par e-mail? Si vous n\'avez pas reçu l\'e-mail, nous nous ferons un plaisir de vous en envoyer un autre.') }}
+            </div>
+
+            <div class="mt-8 px-6 py-4 bg-skin-card shadow-md overflow-hidden sm:rounded-lg">
+                @if (session('status') == 'verification-link-sent')
+                    <div class="rounded-md bg-green-50 p-4 mb-4">
+                        <div class="flex">
+                            <div class="flex-shrink-0">
+                                <x-heroicon-s-check-circle class="h-5 w-5 text-green-400" />
+                            </div>
+                            <div class="ml-3">
+                                <p class="text-sm font-medium text-green-800">
+                                    {{ __('Un nouveau lien de vérification a été envoyé à l\'adresse e-mail que vous avez fournie lors de l\'inscription ou la modification de votre adresse.') }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+
+                <div class="flex items-center justify-between">
+                    <form method="POST" action="{{ route('verification.send') }}">
+                        @csrf
+
+                        <div>
+                            <x-button type="submit">
+                                {{ __('Renvoyer l\'e-mail de vérification') }}
+                            </x-button>
+                        </div>
+                    </form>
+
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <button type="submit" class="underline text-sm text-skin-base hover:text-skin-inverted focus:outline-none">
+                            {{ __('Se déconnecter') }}
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
