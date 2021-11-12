f989311f94a504aa044370c81ccd9b0a0c9b6dfd
Arthur Monney <monneylobe@gmail.com>
2021-11-12 10:34:14 +0100

    :heavy_plus_sign: ajout d'un package de formatage de lien

--- app/helpers.php
+++ app/helpers.php
@@ -32,6 +32,18 @@ if (! function_exists('md_to_html')) {
     }
 }
 
+if (! function_exists('replace_links')) {
+    /**
+     * Convert Standalone Urls to HTML.
+     */
+    function replace_links(string $markdown): string
+    {
+        return (new LinkFinder([
+            'attrs' => ['target' => '_blank', 'rel' => 'nofollow'],
+        ]))->processHtml($markdown);
+    }
+}
+
 if (! function_exists('get_current_theme')) {
     /**
      * Get the current active theme from user settings.
