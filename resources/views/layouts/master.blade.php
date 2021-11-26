f59269be022eed320da581ed0d7a37b3645faaf5
Arthur Monney <monneylobe@gmail.com>
2021-11-27 00:26:40 +0100

    refactoring

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,16 +7,19 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <x-seo::meta />
-
     <title>
         {{ isset($title) ? $title . ' | ' : '' }}
         {{ config('app.name') }}
         {{ is_active('home') ? '- La plus grande communauté de développeurs Laravel & PHP au Cameroun' : '' }}
     </title>
+    <meta name="description" content="Laravel Cameroun est le portail de la communauté de développeurs PHP & Laravel au Cameroun, on partage, on apprend, on découvre et on construit une grande communauté.">
     <meta property="og:site_name" content="Laravel.cm"/>
     <meta property="og:language" content="fr"/>
     <meta name="twitter:author" content="@laravelcm"/>
+    <link rel="canonical" href="{{ $canonical ?? Request::url() }}" />
+
+    @include('layouts._og')
+    <x-seo::meta />
 
     <!-- Styles -->
     <link href="https://fonts.cdnfonts.com/css/operator-mono" rel="stylesheet">
@@ -41,7 +44,6 @@
 
     @include('layouts._favicons')
     @include('layouts._fathom')
-    @include('layouts._og')
 </head>
 <body class="antialiased font-mono bg-skin-body">
 
