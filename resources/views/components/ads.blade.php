90ff37600752369bf88dc8579de6693bed41aebd
Arthur Monney <monneylobe@gmail.com>
2021-11-27 07:56:18 +0100

    ajout de l'image de notification dans le composant ads

--- resources/views/components/ads.blade.php
+++ resources/views/components/ads.blade.php
@@ -1,6 +1,6 @@
 <div id="laravelcm-ads">
     <a href="https://github.com/mckenziearts/laravel-notify/?utm_source=laravelcm&amp;utm_medium=sidebar-widget" class="block">
-        <img class="rounded-lg" src="https://laravel.cm/images/notify.png?25f8fddc09fe5e9f273edaa3ba02bd39" alt="Laravel Notify" width="457" height="336">
+        <img class="rounded-lg" src="{{ asset('/images/notify.png') }}" alt="Laravel Notify" width="457" height="336">
     </a>
     <p class="mt-4 font-normal">
         <a href="https://github.com/mckenziearts/laravel-notify/?utm_source=laravelcm&amp;utm_medium=sidebar-widget" class="text-sm leading-5 text-skin-base">
