144461421832c100631daf575c00e3af9868ba64
mckenziearts <mckenziearts@users.noreply.github.com>
2021-12-01 09:56:26 +0000

    Apply php-cs-fixer changes

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
