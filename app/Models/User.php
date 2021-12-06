70c7cd9323c943d92cd4ca35cbe83c0d8041120e
mckenziearts <mckenziearts@users.noreply.github.com>
2021-12-06 16:41:08 +0000

    Apply php-cs-fixer changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,6 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Cache;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\Permission\Traits\HasRoles;
