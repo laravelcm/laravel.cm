266d9848629c943a63fc698f53f793a655189ed4
Arthur Monney <monneylobe@gmail.com>
2021-12-14 08:38:59 +0100

    :fire: suppression de la neige

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -48,18 +48,6 @@
 <body class="antialiased font-mono bg-skin-body">
 
     <div class="relative overflow-hidden min-h-full">
-        <div class="winter-is-coming">
-
-            <div class="snow snow--near"></div>
-            <div class="snow snow--near snow--alt"></div>
-
-            <div class="snow snow--mid"></div>
-            <div class="snow snow--mid snow--alt"></div>
-
-            <div class="snow snow--far"></div>
-            <div class="snow snow--far snow--alt"></div>
-        </div>
-
         @yield('content')
     </div>
 
