b7bb099458e6411792fc35d192a38417389f082e
mckenziearts <mckenziearts@users.noreply.github.com>
2022-01-17 18:53:41 +0000

    Apply php-cs-fixer changes

--- app/Http/Controllers/Api/ReplyController.php
+++ app/Http/Controllers/Api/ReplyController.php
@@ -15,7 +15,6 @@ use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
-use Illuminate\Support\Facades\Auth;
 
 class ReplyController extends Controller
 {
