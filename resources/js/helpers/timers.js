9d8bd87367e025981da6bc3e141fe712c517ff43
Arthur Monney <monneylobe@gmail.com>
2022-06-08 11:06:53 +0100

    :lipstick: ajout du sticky header

---
+++ resources/js/helpers/timers.js
@@ -0,0 +1,23 @@
+/**
+ * Throttle un callback
+ */
+export function throttle(callback, delay) {
+  let last;
+  let timer;
+  return function () {
+    let context = this;
+    let now = new Date();
+    let args = arguments;
+    if (last && now < last + delay) {
+      // le délai n'est pas écoulé on reset le timer
+      clearTimeout(timer);
+      timer = setTimeout(function () {
+        last = now;
+        callback.apply(context, args);
+      }, delay);
+    } else {
+      last = now;
+      callback.apply(context, args);
+    }
+  };
+}
