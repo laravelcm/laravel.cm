c7e623fa8ded6c5585c9049898af767836c9e059
Arthur Monney <monneylobe@gmail.com>
2022-07-04 19:47:28 +0100

    modification de la orute controller

--- routes/cpanel.php
+++ routes/cpanel.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\Cpanel\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', 'cpanel/home');
-Route::get('/home', [DashboardController::class, 'home'])->name('home');
+Route::get('/home', DashboardController::class)->name('home');
