9e3d22613f467d0f14ffcb583cccd92eb09deac5
Arthur Monney <monneylobe@gmail.com>
2022-11-12 17:05:50 +0100

    :sparkles: Mis en place de la fonctionnalite de rappel et reinitialisation du mot de passe

---
+++ app/Http/Requests/Api/ResetPasswordRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class ResetPasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'token' => 'required',
+            'email' => ['required', 'email', 'exists:users,email'],
+            'password' => [
+                'required',
+                'confirmed',
+                app()->environment('local')
+                    ? Password::min(8)
+                    : Password::min(8)->mixedCase()->numbers()->symbols(),
+            ],
+        ];
+    }
+}
