ed462c9b1dce59eeee41cb412f631ca104e0b84b
Arthur Monney <monneylobe@gmail.com>
2023-01-13 07:17:09 +0100

    Update features list

--- database/seeders/FeatureTableSeeder.php
+++ database/seeders/FeatureTableSeeder.php
@@ -10,23 +10,16 @@ class FeatureTableSeeder extends Seeder
     public function run(): void
     {
         \Feature::add('premium', false);
-        \Feature::add('badge', false);
+        \Feature::add('badges', false);
         \Feature::add('podcasts', false);
+
         \Feature::add('job_skills', false);
         \Feature::add('job_profile', false);
         \Feature::add('auth_login', false);
         \Feature::add('auth_social_login', false);
-        \Feature::add('article_create', false);
-        \Feature::add('article_update', false);
-        \Feature::add('article_delete', false);
-        \Feature::add('discussion_create', false);
-        \Feature::add('discussion_update', false);
-        \Feature::add('discussion_delete', false);
-        \Feature::add('thread_create', false);
-        \Feature::add('thread_update', false);
-        \Feature::add('thread_delete', false);
-        \Feature::add('sponsorship', false);
 
+        \Feature::add('sponsorship', false);
         \Feature::add('preview_feature', false);
+        \Feature::add('view_profile', false);
     }
 }
