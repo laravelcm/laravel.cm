7c95e851fac5debe7dfad832dabe57871051dc36
Arthur Monney <monneylobe@gmail.com>
2023-05-02 04:28:07 +0200

    :arrow_up: Update composer dependencies

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends HttpKernel
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\HttpsProtocol::class,
-        \Fruitcake\Cors\HandleCors::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
