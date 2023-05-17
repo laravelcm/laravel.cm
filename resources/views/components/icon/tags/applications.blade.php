59f589b83d9f212cbf3aebe05d5e6c0a88ddfd6c
Arthur Monney <monneylobe@gmail.com>
2023-05-17 08:37:07 +0200

    Add new tags icon

---
+++ resources/views/components/icon/tags/applications.blade.php
@@ -0,0 +1,4 @@
+<svg {{ $attributes }} fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+    <path d="M24 26H6c-2.2 0-4-1.8-4-4V8c0-2.2 1.8-4 4-4h18c2.2 0 4 1.8 4 4v14c0 2.2-1.8 4-4 4Z" fill="#A576FF" stroke="#A576FF" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M5.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1ZM9.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1ZM13.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1Z" fill="#E3FAFF" stroke="#E3FAFF" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
