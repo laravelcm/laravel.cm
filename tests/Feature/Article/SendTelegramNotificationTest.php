b75850be1b793adf61ab85abdd03bf23aca27980
cybersoldattech <cybersoldattech@users.noreply.github.com>
2024-10-07 18:23:44 +0000

    Fix code styling

--- tests/Feature/Article/SendTelegramNotificationTest.php
+++ tests/Feature/Article/SendTelegramNotificationTest.php
@@ -3,25 +3,25 @@
 declare(strict_types=1);
 
 use App\Livewire\Articles\Create;
-use Livewire\Livewire;
 use App\Models\User;
+use Livewire\Livewire;
 
-test('Send notification on telegram after submition on article',function(){
-// 1- se rassurer que le user est bien connecté
+test('Send notification on telegram after submition on article', function (): void {
+    // 1- se rassurer que le user est bien connecté
     $user = User::factory()->create();
-// 2- soumission d'article par le user connecté
+    // 2- soumission d'article par le user connecté
     Livewire::actingAs($user)->test(Create::class)
-    ->set('title', 'Test Article')
-    ->set('slug', 'test-article')
-    ->set('body', 'This is a test article')
-    ->set('published_at', now())
-    ->set('submitted_at', null)
-    ->set('approved_at', null)
-    ->set('show_toc', true)
-    ->set('canonical_url', 'https://laravel.cm')
-    ->set('associateTags', ['tag1', 'tag2'])
-    ->store();
+        ->set('title', 'Test Article')
+        ->set('slug', 'test-article')
+        ->set('body', 'This is a test article')
+        ->set('published_at', now())
+        ->set('submitted_at', null)
+        ->set('approved_at', null)
+        ->set('show_toc', true)
+        ->set('canonical_url', 'https://laravel.cm')
+        ->set('associateTags', ['tag1', 'tag2'])
+        ->store();
 
-// 3- Envois de la notification au modérateur sur un channel telegram
+    // 3- Envois de la notification au modérateur sur un channel telegram
 
 });
