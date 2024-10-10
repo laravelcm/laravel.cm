7d707d6fde26b278bc225891a514ac8c9ca50d57
Stevy Endaman <stevyabessolo@gmail.com>
2024-10-10 15:03:44 +0200

    Feature/lar 10 schedule notifications (#152)
    * feat: LAR 10 Send a Telegram notification for articles that are submitted but neither approved nor declined.
    
    * refactor: (LAR-10) refactoring some file
    
    * fix: (LAR-10) fix php stan error

---
+++ app/Notifications/PendingArticlesNotification.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Notifications\Messages\MailMessage;
+use NotificationChannels\Telegram\TelegramChannel;
+use NotificationChannels\Telegram\TelegramMessage;
+
+final class PendingArticlesNotification extends Notification
+{
+    use Queueable;
+
+    public function __construct(public Collection $pendingArticles) {}
+
+    public function via(mixed $notifiable): array
+    {
+        return [TelegramChannel::class];
+    }
+
+    public function toTelegram(): TelegramMessage
+    {
+        $message = $this->content();
+
+        return TelegramMessage::create()
+            ->to(config('services.telegram-bot-api.channel'))
+            ->content($message);
+    }
+
+    private function content(): string
+    {
+        $message = __("Pending approval articles:\n\n");
+        foreach ($this->pendingArticles as $article) {
+            $message .= __(
+                "[@:username](:profile_url) submitted the article [:title](:url) on: :date\n\n", [
+                    'username' => $article->user?->username,
+                    'profile_url' => route('profile', $article->user?->username),
+                    'title' => $article->title,
+                    'url' => route('articles.show', $article->slug),
+                    'date' => $article->submitted_at->translatedFormat('d/m/Y')
+                ]
+            );
+        }
+
+        return $message;
+    }
+}
---
+++ tests/Feature/NotifyPendingArticlesTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Console\Commands\NotifyPendingArticles;
+use App\Models\Article;
+use Illuminate\Support\Facades\Notification;
+
+beforeEach(fn() => Notification::fake());
+
+it('will send a notification when there are pending articles', function (): void {
+    Article::factory()->createMany([
+        [
+            'submitted_at' => now(),
+        ],
+        [
+            'submitted_at' => now()->subDay(),
+            'approved_at' => now(),
+        ],
+        [
+            'submitted_at' => now()->subDay(),
+            'declined_at' => now(),
+        ],
+    ]);
+
+    $this->assertDatabaseCount('articles', 3);
+    $this->artisan(NotifyPendingArticles::class)->assertExitCode(0);
+
+    Notification::assertCount(1);
+});
+
+it('will not send a notification when there are no pending articles', function (): void {
+    Article::factory()->createMany([
+        [
+            'submitted_at' => now()->subDay(),
+            'approved_at' => now(),
+        ],
+        [
+            'submitted_at' => now()->subDay(),
+            'declined_at' => now(),
+        ],
+    ]);
+
+    $this->assertDatabaseCount('articles', 2);
+    $this->artisan(NotifyPendingArticles::class)->assertExitCode(0);
+
+    Notification::assertNothingSent();
+    Notification::assertCount(0);
+});
