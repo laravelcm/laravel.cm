7b8a253de6899fda1eb25288764c66296e604a79
Chri$ <chris@learn.edu>
2024-10-16 17:52:42 +0200

    feat:[LAR-32] Add article list wit action  in cpanl

---
+++ app/Filament/Clusters/Articles.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Clusters;
+
+use Filament\Clusters\Cluster;
+
+class Articles extends Cluster
+{
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+}
