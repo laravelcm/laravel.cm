e18e87dbd4c926a9d373740ffd9d72254ffff409
Stephen Jacques <jacquesstephan10@gmail.com>
2024-11-06 17:04:59 +0100

    feat: (LAR-77) Ajout du message d'erreur sur l'interface de connexion et refractoring du code

--- app/Livewire/Forms/LoginForm.php
+++ app/Livewire/Forms/LoginForm.php
@@ -7,26 +7,30 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
+use Livewire\Attributes\Validate;
 use Livewire\Form;
-use Livewire\Component;
 
 class LoginForm extends Form
 {
+    #[Validate('required|string|email')]
     public string $email = '';
+
+    #[Validate('required|string')]
     public string $password = '';
+
+    #[Validate('boolean')]
     public bool $remember = false;
 
-    public function __construct(Component $component = null, string $name = 'form')
-    {
-        $component = $component ?? app(Component::class);
-        parent::__construct($component, $name);
-    }
-
+    /**
+     * Attempt to authenticate the request's credentials.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function authenticate(): void
     {
         $this->ensureIsNotRateLimited();
 
-        if (!Auth::attempt(['email' => $this->email, 'password' => $this->password], $this->remember)) {
+        if (! Auth::attempt($this->only(['email', 'password']), $this->remember)) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
@@ -37,9 +41,12 @@ class LoginForm extends Form
         RateLimiter::clear($this->throttleKey());
     }
 
+    /**
+     * Ensure the authentication request is not rate limited.
+     */
     protected function ensureIsNotRateLimited(): void
     {
-        if (!RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+        if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
             return;
         }
 
@@ -55,8 +62,11 @@ class LoginForm extends Form
         ]);
     }
 
+    /**
+     * Get the authentication rate limiting throttle key.
+     */
     protected function throttleKey(): string
     {
-        return Str::transliterate(Str::lower($this->email) . '|' . request()->ip());
+        return Str::transliterate(Str::lower($this->email).'|'.request()->ip());
     }
 }
--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Session;
 use Livewire\Attributes\Layout;
 use Livewire\Volt\Component;
 
-new #[Layout('layouts.guest')] class extends Component
+new class extends Component
 {
     public LoginForm $form;
 
@@ -22,74 +22,62 @@ new #[Layout('layouts.guest')] class extends Component
         $this->redirectIntended(default: route('dashboard', absolute: false), navigate: true);
     }
 }; ?>
+
     <div>
         <x-container class="flex min-h-full items-center justify-center py-16 sm:pt-24">
             <div class="w-full max-w-md space-y-8">
                 <div>
+                    <x-validation-errors />
                     <h2 class="text-center font-heading text-3xl font-extrabold text-gray-900 dark:text-white">
                         {{ __('pages/auth.login.title') }}
                     </h2>
                 </div>
-                <form class="space-y-6" wire:submit.prevent="login">
+                <form class="space-y-6" wire:submit="login">
                     @csrf
                     <div class="space-y-4">
-                        <div class="space-y-1">
-                            <label for="email" class="block text-sm font-medium text-gray-700 dark:text-gray-300">
-                                {{ __('validation.attributes.email') }}
-                            </label>
-                            <x-text-input 
-                                type="email" 
-                                id="email" 
-                                wire:model="form.email" 
-                                name="email" 
-                                required 
-                                placeholder="{{ __('validation.attributes.email') }}" 
-                                class="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                        <x-filament::input.wrapper>
+                            <x-filament::input
+                                type="text"
+                                id="email-address"
+                                name="email"
+                                autocomplete="email"
+                                required="true"
+                                wire:model="form.email"
+                                aria-label="{{ __('validation.attributes.email') }}"
+                                :placeholder="__('validation.attributes.email')"
                             />
-                            @error('form.email') 
-                                <span class="text-sm text-red-500">{{ $message }}</span> 
-                            @enderror
-                        </div>
-                
-                        <div class="space-y-1">
-                            <label for="password" class="block text-sm font-medium text-gray-700 dark:text-gray-300">
-                                {{ __('validation.attributes.password') }}
-                            </label>
-                            <x-text-input 
-                                type="password" 
-                                id="password" 
+                        </x-filament::input.wrapper>
+                        <x-filament::input.wrapper>
+                            <x-filament::input
+                                type="password"
+                                id="password"
+                                name="password"
+                                required="true"
                                 wire:model="form.password" 
-                                name="password" 
-                                required 
-                                placeholder="{{ __('validation.attributes.password') }}" 
-                                class="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                                aria-label="{{ __('validation.attributes.password') }}"
+                                :placeholder="__('validation.attributes.password')"
                             />
-                            @error('form.password') 
-                                <span class="text-sm text-red-500">{{ $message }}</span> 
-                            @enderror
-                        </div>
+                        </x-filament::input.wrapper>
                     </div>
-                
+
                     <div class="flex items-center justify-between">
                         <div class="flex items-center">
-                            <input 
-                                type="checkbox" 
-                                id="remember_me" 
-                                wire:model="form.remember"
-                                class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                            />
-                            <label for="remember_me" class="ml-2 block text-sm text-gray-500 dark:text-gray-400">
+                            <label class="inline-flex items-center gap-2 cursor-pointer text-sm text-gray-500 dark:text-gray-400">
+                                <x-filament::input.checkbox id="remember_me" name="remember_me" />
                                 {{ __('pages/auth.login.remember_me') }}
                             </label>
                         </div>
-                
+
                         <div class="text-sm">
-                            <a href="{{ route('password.request') }}" class="font-medium text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-white">
+                            <x-link
+                                :href="route('password.request')"
+                                class="font-medium text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-white"
+                            >
                                 {{ __('pages/auth.login.forgot_password') }}
-                            </a>
+                            </x-link>
                         </div>
                     </div>
-                
+
                     <div>
                         <x-buttons.primary type="submit" class="group relative w-full">
                             <span class="absolute pointer-events-none inset-y-0 left-0 flex items-center pl-3">
@@ -99,10 +87,11 @@ new #[Layout('layouts.guest')] class extends Component
                         </x-buttons.primary>
                     </div>
                 </form>
+
                 @include('partials._socials-link')
             </div>
         </x-container>
-    
+
         <x-join-sponsors :title="__('global.sponsor_thanks')" />
     </div>
 
