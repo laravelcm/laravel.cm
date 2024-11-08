320aeb9ac35ea9658b11d5420d193c0de385544b
Stevy Endaman <stevyabessolo@gmail.com>
2024-11-08 22:49:31 +0100

    refactor(test): (LAR-21) remove unecessary coma

--- tests/Feature/Article/PostArticleToTwitterTest.php
+++ tests/Feature/Article/PostArticleToTwitterTest.php
@@ -7,7 +7,6 @@ use Spatie\TestTime\TestTime;
 use Illuminate\Support\Facades\Notification;
 use App\Console\Commands\PostArticleToTwitter;
 
-
 beforeEach(fn () => 
 Notification::fake(), 
 TestTime::freeze('Y-m-d H:i:s', '2021-05-01 00:00:01')
@@ -16,10 +15,10 @@ TestTime::freeze('Y-m-d H:i:s', '2021-05-01 00:00:01')
 describe(PostArticleToTwitter::class, function() {
     it('shares one article on Twitter every 4 hours when the artisan command runs', function (): void {
         Article::factory()->createMany([
-            ['submitted_at' => now(),],
-            ['submitted_at' => now(), 'approved_at' => now(),  'published_at' => now(),],
-            ['submitted_at' => now(), 'approved_at' => now(),  'published_at' => now()->addHours(1),],
-            ['submitted_at' => now(), 'declined_at' => now(),],
+            ['submitted_at' => now()],
+            ['submitted_at' => now(), 'approved_at' => now(),  'published_at' => now()],
+            ['submitted_at' => now(), 'approved_at' => now(),  'published_at' => now()->addHours(1)],
+            ['submitted_at' => now(), 'declined_at' => now()],
         ]);
     
         $this->assertDatabaseCount('articles', 4);
@@ -38,8 +37,8 @@ describe(PostArticleToTwitter::class, function() {
     
     it('will not send article when there are not articles to share', function (): void {
         Article::factory()->createMany([
-            ['submitted_at' => now(),'approved_at' => now(),'shared_at' => now(),],
-            ['submitted_at' => now(),'approved_at' => now(),'shared_at' => now(),],
+            ['submitted_at' => now(),'approved_at' => now(),'shared_at' => now()],
+            ['submitted_at' => now(),'approved_at' => now(),'shared_at' => now()],
         ]);
     
         $this->assertDatabaseCount('articles', 2);
