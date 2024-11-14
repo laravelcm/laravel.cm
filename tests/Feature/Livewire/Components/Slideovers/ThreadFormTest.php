2d80eea8d7cdccceca0881583528d0a8918831bb
Arthur Monney <monneylobe@gmail.com>
2024-11-14 20:35:13 +0100

    wip

--- tests/Feature/Livewire/Components/Slideovers/ThreadFormTest.php
+++ tests/Feature/Livewire/Components/Slideovers/ThreadFormTest.php
@@ -14,103 +14,106 @@ beforeEach(function (): void {
     Notification::fake();
 });
 
-it('return redirect to unauthenticated user', function (): void {
-    Livewire::test(ThreadForm::class)
-        ->assertStatus(302);
-})->group('thread');
+/**
+ * @var \Tests\TestCase $this
+ */
+describe(ThreadForm::class, function (): void {
+    it('return redirect to unauthenticated user', function (): void {
+        Livewire::test(ThreadForm::class)
+            ->assertStatus(302);
+    });
 
-it('render the component when authenticated user', function (): void {
-    $this->login();
+    it('render the component when authenticated user', function (): void {
+        $this->login();
 
-    Livewire::test(ThreadForm::class)
-        ->assertStatus(200);
-})->group('thread');
+        Livewire::test(ThreadForm::class)
+            ->assertSuccessful();
+    });
 
-it('validate forms input', function (): void {
-    $this->login();
+    it('validate forms input', function (): void {
+        $this->login();
 
-    Livewire::test(ThreadForm::class)
-        ->fillForm([
-            'title' => '',
-            'body' => '',
-            'channels' => [],
-        ])
-        ->call('save')
-        ->assertHasFormErrors([
-            'title' => ['required'],
-            'body' => ['required'],
-        ]);
-})->group('thread');
+        Livewire::test(ThreadForm::class)
+            ->fillForm([
+                'title' => '',
+                'body' => '',
+                'channels' => [],
+            ])
+            ->call('save')
+            ->assertHasFormErrors([
+                'title' => ['required'],
+                'body' => ['required'],
+            ]);
+    });
 
-it('validate channels can extends 3 when create a thread', function (): void {
-    $this->login();
+    it('validate channels can extends 3 when create a thread', function (): void {
+        $this->login();
 
-    Livewire::test(ThreadForm::class)
-        ->fillForm([
-            'title' => 'I have a question about laravel Cameroun',
-            'body' => 'this is my kind body',
-            'channels' => ['laravel', 'php', 'tailwind', 'gaming'],
-        ])
-        ->call('save')
-        ->assertHasFormErrors([
-            'channels' => ['max'],
-        ]);
-})->group('thread');
+        Livewire::test(ThreadForm::class)
+            ->fillForm([
+                'title' => 'I have a question about laravel Cameroun',
+                'body' => 'this is my kind body',
+                'channels' => ['laravel', 'php', 'tailwind', 'gaming'],
+            ])
+            ->call('save')
+            ->assertHasFormErrors([
+                'channels' => ['max'],
+            ]);
+    });
 
-it('user can create a new thread', function (): void {
-    $user = $this->login();
-    $channels = Channel::factory()->count(3)->create();
+    it('user can create a new thread', function (): void {
+        $user = $this->login();
+        $channels = Channel::factory()->count(3)->create();
 
-    Livewire::test(ThreadForm::class)
-        ->fillForm([
-            'title' => 'I have a question about laravel Cameroun',
-            'body' => 'this is my kind body',
-            'channels' => $channels->pluck('id')->toArray(),
-        ])
-        ->call('save')
-        ->assertHasNoFormErrors();
+        Livewire::test(ThreadForm::class)
+            ->fillForm([
+                'title' => 'I have a question about laravel Cameroun',
+                'body' => 'this is my kind body',
+                'channels' => $channels->pluck('id')->toArray(),
+            ])
+            ->call('save')
+            ->assertHasNoFormErrors();
 
-    $thread = Thread::query()->first();
+        $thread = Thread::query()->first();
 
-    expect($thread?->user)->toBeInstanceOf(User::class)
-        ->and($thread?->user->is($user))
-        ->toBeTrue();
-})->group('thread');
+        expect($thread?->user)->toBeInstanceOf(User::class)
+            ->and($thread?->user->is($user))
+            ->toBeTrue();
+    });
 
-it('user cannot create thread with and unverified email address', function (): void {
-    $user = $this->createUser(['email_verified_at' => null]);
-    $channels = Channel::factory()->count(2)->create();
+    it('user cannot create thread with and unverified email address', function (): void {
+        $user = $this->createUser(['email_verified_at' => null]);
+        $channels = Channel::factory()->count(2)->create();
 
-    $this->actingAs($user);
+        $this->actingAs($user);
 
-    Livewire::test(ThreadForm::class)
-        ->fillForm([
-            'title' => 'I have a question about laravel Cameroun',
-            'body' => 'this is my kind body',
-            'channels' => $channels->pluck('id')->toArray(),
-        ])
-        ->call('save');
+        Livewire::test(ThreadForm::class)
+            ->fillForm([
+                'title' => 'I have a question about laravel Cameroun',
+                'body' => 'this is my kind body',
+                'channels' => $channels->pluck('id')->toArray(),
+            ])
+            ->call('save');
 
-    expect(Thread::query()->first())
-        ->toBeNull();
-})
-    ->group('thread')
-    ->expectException(UnverifiedUserException::class);
+        expect(Thread::query()->first())
+            ->toBeNull();
+    })->expectException(UnverifiedUserException::class);
 
-it('user cannot updated a thread that is not author', function (): void {
-    $this->login();
+    it('user cannot updated a thread that is not author', function (): void {
+        $this->login();
 
-    $author = User::factory()->create();
-    $thread = Thread::factory()->create(['user_id' => $author->id]);
-    $channels = Channel::factory()->count(3)->create();
+        $author = User::factory()->create();
+        $thread = Thread::factory()->create(['user_id' => $author->id]);
+        $channels = Channel::factory()->count(3)->create();
 
-    $thread->channels()->attach($channels->modelKeys());
+        $thread->channels()->attach($channels->modelKeys());
 
-    Livewire::test(ThreadForm::class, ['threadId' => $thread->id])
-        ->fillForm([
-            'title' => 'Updated thread question',
-            'body' => 'this is my kind body updated',
-        ])
-        ->call('save')
-        ->assertStatus(403);
+        Livewire::test(ThreadForm::class, ['threadId' => $thread->id])
+            ->fillForm([
+                'title' => 'Updated thread question',
+                'body' => 'this is my kind body updated',
+            ])
+            ->call('save')
+            ->assertStatus(403);
+    });
 })->group('thread');
