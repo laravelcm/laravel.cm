e3f1d18351aa740b56fb38392772acd380f91484
Arthur Monney <monneylobe@gmail.com>
2024-11-19 10:06:51 +0100

    style: add filament style

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+        'resources/css/filament/admin/theme.css',
+      ],
       refresh: [...refreshPaths, 'app/Livewire/**'],
     }),
     {
