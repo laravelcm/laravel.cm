e934196a5bda15f0945b3d58fe35f4d9199e7d9e
Chri$ <samorytakougne@gmail.com>
2024-11-28 19:35:18 +0100

    fix:[LAR-119] fix non-existent route link and translate page

---
+++ lang/fr/pages/notification.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    'empty' => 'Vous n\'avez pas de notifications non lues.',
+    'new_content' => 'Lancer un nouveau contenu forum ?',
+    'choose_content' => ' Commencer par choisir quel type de contenu vous voulez créer.',
+    'article_action' => 'Rédiger un article',
+    'share_content' => 'Partager un tutoriel ou une information importante.',
+    'start_discussion' => ' Démarrer une discussion',
+    'start_discussion_description_text' => 'Vous souhaitez apporter une pierre à l\'édifice.',
+    'ask_help' => 'Demander de l\'aide',
+    'ask_help_description' => ' Vous avez un problème ? Trouvons ensemble une solution.',
+];
