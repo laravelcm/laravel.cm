03ff18861e3996f15f37e6481dcec99a57bea68a
Chri$ <samorytakougne@gmail.com>
2024-11-28 19:35:18 +0100

    fix:[LAR-119] fix non-existent route link and translate page

---
+++ lang/en/pages/notification.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    'empty' => 'You have no unread notifications.',
+    'new_content' => 'Start new forum content ?',
+    'choose_content' => 'Start by choosing the type of content you want to create.',
+    'article_action' => 'Write an article',
+    'share_content' => 'Share a tutorial or important information.',
+    'start_discussion' => 'Start a discussion.',
+    'start_discussion_description_text' => 'You\'d like to make a contribution.',
+    'ask_help' => 'Ask for help',
+    'ask_help_description' => 'Got a problem? Let\'s find a solution together.',
+];
