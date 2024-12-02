168492eb9199f76e984c73a57dbe861f24c2d4d1
Chri$ <samorytakougne@gmail.com>
2024-12-03 00:40:04 +0100

    add space in End of array, change font to font-heading

--- lang/en/pages/notification.php
+++ lang/en/pages/notification.php
@@ -13,4 +13,5 @@ return [
     'start_discussion_description_text' => 'You\'d like to make a contribution.',
     'ask_help' => 'Ask for help',
     'ask_help_description' => 'Got a problem? Let\'s find a solution together.',
+
 ];
