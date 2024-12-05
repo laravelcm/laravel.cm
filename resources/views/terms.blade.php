69125d80b0f3ff8d80775d8ca9b96d5009c430d3
Chri$ <samorytakougne@gmail.com>
2024-12-05 01:08:27 +0100

    translate terms page

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,81 +1,59 @@
-<x-app-layout title="Conditions d'utilisation">
+<x-app-layout title="{{ __('pages/terms.title') }}">
     <div class="prose relative mx-auto max-w-4xl px-4 py-10">
-        <h1>Conditions d'utilisation</h1>
+        <h1>{{ __('pages/terms.title') }}</h1>
         <p>
-            <span class="text-xs font-medium uppercase text-skin-muted">Dernière mise à jour : 08 Juin 2019</span>
+            <span class="text-xs font-medium uppercase text-skin-muted">{{ __('pages/terms.last_maj') }}</span>
         </p>
         <p>
-            Les présentes conditions générales d'utilisation du site Web écrites sur cette page Web géreront votre
-            utilisation de notre site Web, Laravel Cameroun accessible à laravel.cm
+            {{ __('pages/terms.first_description') }}
         </p>
         <p>
-            Ces conditions seront pleinement appliquées et affecteront votre utilisation de ce site Web. En utilisant ce
-            site Web, vous avez accepté toutes les modalités et conditions écrites ici. Vous ne devez pas utiliser ce
-            site Web si vous n'êtes pas d'accord avec l'une de ces conditions générales standard du site Web. Les
-            éléments contenus dans ce site Web sont protégés par le droit d'auteur et la loi sur les marques
-            applicables.
+            {{ __('pages/terms.second_description') }}
         </p>
 
-        <h2>Droits de propriété intellectuelle</h2>
+        <h2>{{ __('pages/terms.intellectual_property_rights.title') }}</h2>
         <p>
-            Outre le contenu que vous possédez, en vertu des présentes Conditions, Laravel Cameroun et/ou ses concédants
-            de licence détiennent tous les droits de propriété intellectuelle et les matériaux contenus dans ce site
-            Web.
+            {{ __('pages/terms.intellectual_property_rights.first_paragraph') }}
         </p>
-        <p>Vous disposez d'une licence limitée uniquement aux fins de visualisation du contenu de ce site Web.</p>
+        <p>{{ __('pages/terms.intellectual_property_rights.second_paragraph') }}</p>
 
-        <h2>Votre contenu</h2>
+        <h2>{{ __('pages/terms.content.title') }}</h2>
         <p>
-            Dans les présentes conditions générales du site Web, « votre contenu » signifie tout audio, texte vidéo,
-            images ou autre matériel que vous choisissez d'afficher sur ce site Web. En affichant Votre Contenu, vous
-            accordez à Laravel Cameroun une licence non exclusive, irrévocable et sous licence mondiale pour l'utiliser,
-            la reproduire, l'adapter, la publier, la traduire et la distribuer sur tous les supports.
+            {{ __('pages/terms.content.first_paragraph') }}
         </p>
         <p>
-            Votre contenu doit être le vôtre et ne doit pas empiéter sur les droits de tiers. Laravel Cameroun se
-            réserve le droit de supprimer tout contenu de ce site Web à tout moment sans préavis.
+            {{ __('pages/terms.content.second_paragraph') }}
         </p>
 
-        <h2>Limitations</h2>
+        <h2>{{ __('pages/terms.limitations.title') }}</h2>
         <p>
-            En aucun cas, Laravel Cameroun, ni aucun de ses dirigeants, administrateurs et employés, ne sera tenu
-            responsable de tout ce qui découle de ou de quelque manière que ce soit lié à votre utilisation de ce site
-            Web, que cette responsabilité soit contractuelle.
+            {{ __('pages/terms.limitations.first_paragraph') }}
         </p>
         <p>
-            Laravel Cameroun, y compris ses dirigeants, administrateurs et employés, ne pourra être tenu responsable de
-            toute responsabilité indirecte, consécutive ou spéciale découlant de ou liée de quelque manière que ce soit
-            à votre utilisation de ce site Web.
+            {{ __('pages/terms.limitations.second_paragraph') }}
         </p>
 
-        <h2>Indemnités</h2>
+        <h2>{{ __('pages/terms.indemnities.title') }}</h2>
         <p>
-            Par la présente, vous indemnisez Laravel Cameroun dans toute la mesure de et contre toute responsabilité,
-            les coûts, les demandes, les causes d'action, les dommages et dépenses découlant de quelque manière que ce
-            soit de votre violation de l'une des dispositions des présentes Conditions.
+            {{ __('pages/terms.indemnities.description') }}
         </p>
 
-        <h2>Modifications des termes</h2>
+        <h2>{{ __('pages/terms.update_terms.title') }}</h2>
         <p>
-            Laravel Cameroun est autorisé à réviser ces conditions à tout moment comme bon lui semble, et en utilisant
-            ce site Web, vous êtes censé revoir ces conditions régulièrement.
+            {{ __('pages/terms.update_terms.description') }}
         </p>
 
-        <h2>Affectation</h2>
+        <h2>{{ __('pages/terms.affectation.title') }}</h2>
         <p>
-            Laravel Cameroun est autorisé à céder, transférer et sous-traiter ses droits et/ou obligations en vertu des
-            présentes Conditions sans aucune notification. Cependant, vous n'êtes pas autorisé à céder, transférer ou
-            sous-traiter aucun de vos droits et/ou obligations en vertu des présentes Conditions.
+            {{ __('pages/terms.affectation.description') }}
         </p>
 
-        <h2>Juridiction</h2>
+        <h2>{{ __('pages/terms.juridictions.title') }}</h2>
         <p>
-            Ces conditions seront régies et interprétées conformément aux lois de l'État du Cameroun, et vous vous
-            soumettez à la juridiction non exclusive des tribunaux d'État et fédéraux situés au Cameroun pour la
-            résolution de tout litige.
+            {{ __('pages/terms.juridictions.description') }}
         </p>
 
-        <h2>Contactez nous</h2>
-        <p>Si vous avez des questions concernant ces Conditions, veuillez nous contacter.</p>
+        <h2>{{ __('pages/terms.contact_us.title') }}</h2>
+        <p>{{ __('pages/terms.contact_us.description') }}</p>
     </div>
 </x-app-layout>
