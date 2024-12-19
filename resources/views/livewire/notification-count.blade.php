4410e4c28c6e42670d095f2bd3bd8082784c995e
Chri$ <samorytakougne@gmail.com>
2024-12-19 03:02:21 +0100

    lar:[137] change notification count and indicator to volt component
    feat[lar-137] Change count and indicator livewire component to  volt component
    
    feat:[lar-137] Change count and indicator livewire component to volt component

--- resources/views/livewire/notification-count.blade.php
+++ resources/views/livewire/notification-count.blade.php
@@ -1,3 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use function Livewire\Volt\{on};
+use Illuminate\Support\Facades\Auth;
+
+use function Livewire\Volt\{state};
+
+state(['count' => Auth::user()->unreadNotifications()->count()]);
+
+on(['NotificationMarkedAsRead' => function (int $count) {
+    return $count;
+}]);
+
+?>
+
 <span class="rounded-full bg-green-100 px-3 text-base text-green-500">
     {{ $count }}
 </span>
--- resources/views/livewire/notification-indicator.blade.php
+++ resources/views/livewire/notification-indicator.blade.php
@@ -1,3 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use function Livewire\Volt\{on};
+use Illuminate\Support\Facades\Auth;
+
+use function Livewire\Volt\{state};
+
+state(['hasNotification' => Auth::user()->unreadNotifications()->count() > 0 ]);
+
+on(['NotificationMarkedAsRead' => function (int $count) {
+    return $count > 0 ;
+}]);
+
+?>
+
 <span
-    class="{{ $hasNotification ? 'shadow-solid absolute right-0 top-0 block size-2 rounded-full bg-primary-600 text-white' : 'hidden' }}"
+    @class([
+        'hidden' => !$hasNotification,
+        'shadow-solid absolute right-0 top-0 block size-2 rounded-full bg-primary-600 text-white' => $hasNotification
+    ])
 ></span>
