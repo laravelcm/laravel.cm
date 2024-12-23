40cde56d3c974465c292d4b7ad7e3675735203b8
Chri$ <samorytakougne@gmail.com>
2024-12-23 19:49:07 +0100

    feat:[lar-143] Translate faq age

---
+++ lang/en/pages/faq.php
@@ -0,0 +1,161 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    'title' => 'Frequently asked questions',
+    'description' => 'Some of this information is not frequently requested, but it\'s always good to know.',
+    'rules' => 'code of conduct',
+
+    'card' => [
+
+        'one' => [
+            'title' => 'Who can publish articles/topics on Laravel.cm?',
+
+            'first_description' => ' Moderators! Yes, you have permission to publish a new article, whatever it is
+                            as long as it complies with our community guidelines and passes through the common-sense
+                            common-sense spam filters.',
+
+            'second_description' => ' Your post may be deleted at the discretion of the moderators if they feel it does not
+                            does not meet the requirements of our',
+
+            'tirdh_description' => 'But after validation, your article can be made public on the community\'s news feed
+and sent as a notification to the community\'s Twitter account.',
+        ],
+        'two' => [
+            'title' => 'Who can publish on Laravel.cm?',
+            'first_description' => 'Everyone! Yes, you have permission to publish new content of any kind,
+                            as long as it complies with our community guidelines and passes the common-sense
+                            common-sense spam filters.',
+            'second_description' => 'Your content may be removed at the discretion of the moderators if they feel it does not
+                            does not meet the requirements of our',
+
+            'rules' => 'code of conduct',
+        ],
+        'three' => [
+            'title' => 'Where can I find the community?',
+            'sub_description' => 'The community is present on Twitter, Github, LinkedIn, Facebook and YouTube. All links
+                            links are available in the footer.',
+            'first_description' => 'But when it comes to communication channels, the main channel for communication and exchange with
+                            community members remains the group',
+            'second_description' => 'You can join slack here',
+            'third_paragraph_part_one' => 'But the community also has a',
+            'third_paragraph_part_two' => ' and a WhatsApp group (limited by WhatsApp group management rules) that can be accessed by anyone.',
+            'four_paragraph' => 'That said, we recommend joining the Slack group more.',
+        ],
+        'four' => [
+            'title' => 'How does sponsorship work on Laravel.cm?',
+            'first_description' => ' Sponsoring on Laravel.cm is an opportunity to gain visibility and put your content
+                            your content on the site.',
+            'second_description_part_one' => 'Visit the',
+            'second_description_part_two' => 'to get all the information you need (e.g. how it works, how to become a community sponsor
+                            community, why sponsor your content, etc.).',
+        ],
+        'five' => [
+            'title' => ' Can I take part in a podcast?',
+            'first_description' => ' Of course! The podcasts will be open to all, and everyone will have the opportunity to
+                            participate.',
+            'second_description' => 'The community aims to be open and not just cover the subject of programming.',
+            'trhird_description' => 'Themes will also be shared to give everyone the chance to share their
+                            experience around a microphone.',
+        ],
+        'six' => [
+            'title' => 'How do I change my Twitter/GitHub username?',
+            'first_description_one' => 'You can add or remove Twitter/GitHub associations from your settings.
+                            note that you can only do so if both Twitter and GitHub are connected to your account. If
+                            problems with this, send an e-mail to',
+            'first_description_two' => ' and we\'ll take care of it.',
+        ],
+        'seven' => [
+            'title' => 'How to become Premium ?',
+            'description' => 'Becoming premium on Laravel.cm means supporting the creation of new content and gaining access to
+                            exclusive content to learn and improve (such as video downloads of source code
+                            and designs).',
+            'paragraph_part_one' => 'To become premium, you must go to the page for',
+            'paragraph_link' => 'Become premium',
+            'paragraph_part_two' => 'and choose a subscription',
+        ],
+        'height' => [
+            'title' => 'How to delete my account ?',
+            'description' => 'You will find the option to delete your account in your settings.
+                            Auto deletion will delete your profile on Laravel.cm, as well as all articles,
+                            comments, discussions, topics, etc.',
+        ],
+        'nine' => [
+            'title' => 'Which technologies are used Laravel.cm ?',
+            'first_description_part_one' => 'Laravel.cm is based on the Laravel framework and implements the',
+            'first_description_part_two' => 'which lets you create responsive applications on Laravel with Livewire and Alpinejs.',
+            'second_description' => 'It\'s a full-stack development preset, built by members of the
+                            Laravel community.',
+        ],
+        'ten' => [
+            'title' => 'How to report abuse ?',
+            'first_description' => 'For a specific comment: access the comment and click on the drop-down arrow
+                            to report abuse.',
+            'second_description' => 'For a specific article: access the article, scroll down and click on
+                            report abuse.',
+            'third_description' => 'In general, you can send an e-mail to',
+        ],
+        'eleven' => [
+            'title' => 'Can I follow the RSS feeds on Laravel.cm?',
+            'description_part_one' => 'Oui, vous pouvez. Le lien de notre flux principal se trouve ici :',
+            'description_part_two' => '. Pour les flux spécifiques aux utilisateurs, vous pouvez les trouver via
+                            https://laravel.cm/feed/username.',
+        ],
+        'twelve' => [
+            'title' => 'Can I take part in a podcast?',
+            'first_description' => 'Of course! The podcasts will be open to all, and everyone will have the opportunity to
+                            participate.',
+            'second_description' => 'The community aims to be open and not just cover the subject of programming.',
+            'thrid_description' => 'Themes will also be shared to give everyone the chance to share their
+                            experience around a microphone.',
+        ],
+        'thirteen' => [
+            'title' => 'Can I post where I can find a job on Laravel.cm?',
+            'description_one' => 'Yes, the community will also be used by companies to post offers and recruit talent
+                            talents, freelancers, interns for assignments, contracts, etc.',
+            'description_part_two' => 'Laravel.cm aims to put developers from the French-speaking world in the spotlight
+                            that can be shared across Africa and the world in general.
+                            in general.',
+        ],
+        'fourteen' => [
+            'title' => 'How to change my Twitter/GitHub username ?',
+            'first_description' => 'You can add or remove Twitter/GitHub associations from your settings.
+                            note that you can only do so if both Twitter and GitHub are connected to your account. If
+                            problems with this, send an e-mail to',
+            'second_description' => 'and we\'ll take care of it.',
+        ],
+        'fifteen' => [
+            'title' => 'How can I become Premium?',
+            'first_description' => 'Becoming premium on Laravel.cm means supporting the creation of new content and gaining access to
+                            exclusive content to learn and improve (such as video downloads of source code
+                            and designs).',
+            'second_description_part_one' => 'To become premium, you must go to the page for',
+            'become_premium' => 'Become premium',
+            'second_description_part_two' => 'and choose a subscription.',
+        ],
+        'sixteen' => [
+            'title' => 'How does sponsorship work on Laravel.cm?',
+            'description_one' => 'Sponsoring on Laravel.cm is an opportunity to gain visibility and put your content
+                            your content on the site.',
+            'description_two_part_one' => 'Visit the',
+            'description_two_part_two' => 'to get all the information you need (e.g. how it works, how to become a community sponsor
+                            community, why sponsor your content, etc.).',
+        ],
+        'nineteen' => [
+            'title' => 'How sponsoring works on Laravel.cm ?',
+            'description' => 'Sponsoring on Laravel.cm is an opportunity to gain visibility and put your content
+                            your content on the site.',
+            'description_two' => 'But when it comes to communication channels, the main channel for communication and exchange with
+                            community members remains the group',
+            'description_two_text_one' => '. You can join slack here',
+            'description_three_text' => 'But the community also has a',
+            'description_two_text_two' => 'and a WhatsApp group (limited by WhatsApp\'s group management rules) which are
+                            accessible to all.',
+            'description_four' => 'That said we, more recommend joining the Slack group.',
+        ],
+
+    ],
+
+];
