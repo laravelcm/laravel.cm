83ce90fb302a0311cfa1d614e3102a49dd641c7d
Endaman Stevy <stevyabessolo@gmail.com>
2025-01-05 19:36:32 +0100

    feat:(LAR-159) BO add badge status to approbation

--- app/Filament/Resources/ArticleResource.php
+++ app/Filament/Resources/ArticleResource.php
@@ -7,6 +7,8 @@ namespace App\Filament\Resources;
 use App\Actions\Article\ApprovedArticleAction;
 use App\Filament\Resources\ArticleResource\Pages;
 use App\Models\Article;
+use Awcodes\FilamentBadgeableColumn\Components\Badge;
+use Awcodes\FilamentBadgeableColumn\Components\BadgeableColumn;
 use Filament\Resources\Resource;
 use Filament\Support\Enums\MaxWidth;
 use Filament\Tables;
@@ -67,36 +69,39 @@ final class ArticleResource extends Resource
                     ->label('Soumission')
                     ->placeholder('N/A')
                     ->date(),
-                Tables\Columns\TextColumn::make('status')
+                BadgeableColumn::make('status')
                     ->label('Statut')
-                    ->badge()
                     ->getStateUsing(function ($record) {
                         if ($record->approved_at) {
-                            return 'Approuvé - '.$record->approved_at->format('d/m/Y');
+                            return $record->approved_at->format('d/m/Y');
                         }
 
                         if ($record->declined_at) {
-                            return 'Décliné - '.$record->declined_at->format('d/m/Y');
+                            return $record->declined_at->format('d/m/Y');
                         }
 
-                        return 'En attente';
+                        return 'depuis le '.$record->submitted_at->format('d/m/Y');
                     })
-                    ->colors([
-                        'success' => fn ($state) => str_contains($state, 'Approuvé'),
-                        'danger' => fn ($state) => str_contains($state, 'Décliné'),
-                        'warning' => fn ($state) => $state === 'En attente',
-                    ])
-                    ->icon(
-                        fn ($state) => str_contains($state, 'Approuvé') ? 'heroicon-s-check-circle' :
-                        (str_contains($state, 'Décliné') ? 'heroicon-s-x-mark-circle' :
-                        ($state === 'En attente' ? 'heroicon-s-clock' : null))
-                    )
+                    ->prefixBadges(function ($record) {
+                        if ($record->approved_at) {
+                            return [
+                                Badge::make('Approuvé')
+                                    ->color('success'),
+                            ];
+                        }
+
+                        if ($record->declined_at) {
+                            return [
+                                Badge::make('Décliné')
+                                    ->color('danger'),
+                            ];
+                        }
+
+                        return [Badge::make('En attente')
+                            ->color('warning')];
+                    })
+                    ->searchable()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('declined_at')
-                    ->label('Décliner')
-                    ->placeholder('N/A')
-                    ->date()
-                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->actions([
                 ActionGroup::make([
