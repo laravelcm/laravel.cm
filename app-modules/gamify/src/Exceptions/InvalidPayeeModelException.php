522755e5cdad4d6007961d0748b4ccf5ca6c81c5
StevyEndaman <stevyabessolo@gmail.com>
2025-04-18 17:53:09 +0100

    fix: resolve some phpstan error

--- app-modules/gamify/src/Exceptions/InvalidPayeeModelException.php
+++ app-modules/gamify/src/Exceptions/InvalidPayeeModelException.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Laravelcm\Gamify\Exceptions;
 
-final class InvalidPayeeModelException extends \Exception 
+final class InvalidPayeeModelException extends \Exception
 {
     protected $message = 'payee() method must return a model which will get the points.';
 }
