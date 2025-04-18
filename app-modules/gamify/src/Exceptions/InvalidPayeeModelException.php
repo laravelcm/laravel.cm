bd32d2792c9ecf5a473b2ddab62fd025483bcdf3
StevyEndaman <stevyabessolo@gmail.com>
2025-04-18 17:51:50 +0100

    feat: add <server name=DB_CONNECTION value=sqlite/> missing

--- app-modules/gamify/src/Exceptions/InvalidPayeeModelException.php
+++ app-modules/gamify/src/Exceptions/InvalidPayeeModelException.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Laravelcm\Gamify\Exceptions;
 
-final class InvalidPayeeModelException extends \Exception
+final class InvalidPayeeModelException extends \Exception 
 {
-    protected string $message = 'payee() method must return a model which will get the points.';
+    protected $message = 'payee() method must return a model which will get the points.';
 }
