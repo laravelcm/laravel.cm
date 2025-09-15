b0c709ef22ad760f1d2de8dde85e1c7c94983901
Arthur Monney <monneylobe@gmail.com>
2025-09-15 11:52:17 +0200

    feat: Create s3 disk migrate command

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,6 +57,21 @@ return [
             'throw' => false,
         ],
 
+        'media-s3' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'root' => env('AWS_ROOT', 'public'),
+            'visibility' => 'public',
+            'directory_visibility' => 'public',
+            'throw' => false,
+        ],
+
         'media' => [
             'driver' => 'local',
             'root' => public_path('media'),
