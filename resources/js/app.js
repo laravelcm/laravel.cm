4bcbcd399a697adb28be3348bc9276512bbe17e3
Arthur Monney <monneylobe@gmail.com>
2026-01-07 13:32:25 +0100

    fix: import of livewire markdown editor on app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import "@tailwindplus/elements";
 
-import "../../app-modules/markdown-editor/resources/js/markdown-editor";
+import "../../app-modules/livewire-markdown-editor/resources/js/markdown-editor";
 import "../../vendor/laravelcm/livewire-slide-overs/resources/js/slide-over";
 import "./utils/helpers";
 import "./utils/scrollspy";
