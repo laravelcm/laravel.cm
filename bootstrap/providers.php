05e05dfddcd06de3ac7c16eb23103ce3d7d993d8
Arthur Monney <monneylobe@gmail.com>
2026-03-18 11:23:22 +0100

    refactor: remove livewire/volt and convert to Livewire 4 single-file components
    Volt functional components converted to native Livewire 4 class-based
    single-file syntax (new class extends Component). Removed VoltServiceProvider
    and uninstalled livewire/volt dependency.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -6,5 +6,4 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\EventServiceProvider::class,
     App\Providers\Filament\CpanelPanelProvider::class,
-    App\Providers\VoltServiceProvider::class,
 ];
